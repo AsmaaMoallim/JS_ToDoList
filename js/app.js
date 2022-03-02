b4f4c375452cbb23d754b457c966d1611aa373cc
AsmaaMoallim <asmaaam1o1@gmail.com>
2022-03-02 12:13:13 +0300

    course code

--- js/app.js
+++ js/app.js
@@ -1,24 +1,31 @@
 const addform = document.querySelector(".add");
 const todos = document.querySelector(".todos");
+const searchform = document.querySelector(".search");
 
+// create li template
+const generateTemplate = (todo) => {
+  const html = `<li class="list-group-item text-light d-flex justify-content-between align-items-cen">
+                <span>${todo}
+                </span>
+                <i class="far fa-trash-alt delete"></i></li>`;
+
+  todos.innerHTML += html;
+};
+
+// add todo
 addform.addEventListener("submit", (e) => {
   e.preventDefault();
+  Todo = addform.add.value.trim();
 
-  Todo = `<li class="list-group-item text-light d-flex justify-content-between align-items-cen">
-                <span>${addform.add.value}
-                </span>
-                <i class="far fa-trash-alt delete"></i>
-        </li>`;
-
-  todos.innerHTML += Todo;
-  addform.add.value = "";
-
+  if (Todo.length) {
+    generateTemplate(addform.add.value.trim());
+    addform.reset(); // this is new
+  }
 });
+
+// delete todo
 todos.addEventListener("click", (e) => {
-  let classlis = e.target.getAttribute("class");
-  if (classlis) {
-    if (classlis.includes("delete")) {
-      e.target.parentNode.remove();
-    }
+  if (e.target.classList.contains("delete")) {
+    e.target.parentNode.remove();
   }
 });
